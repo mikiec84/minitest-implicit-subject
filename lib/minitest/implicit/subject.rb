39d8653ffbab8b9d87fff0d944b508165f7e33b4
Anton Lindqvist <anton@qvister.se>
2013-03-08 23:13:51 +0100

    Initial commit.

---
+++ lib/minitest/implicit/subject.rb
@@ -0,0 +1,9 @@
+require "minitest/implicit/subject/version"
+
+module Minitest
+  module Implicit
+    module Subject
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/minitest/implicit/subject/version.rb
@@ -0,0 +1,7 @@
+module Minitest
+  module Implicit
+    module Subject
+      VERSION = "0.0.1"
+    end
+  end
+end
